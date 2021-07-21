b84eef29abb10716433296abc14879cf8df6613a
gejiawangluo <gejiawangluo@aadeMacBook-Pro.local>
2021-07-21 16:51:13 +0800

    asd

--- nav-01/src/main.js
+++ nav-01/src/main.js
@@ -1,24 +1,49 @@
 const $siteList = $('.siteList');
 const $lastLi = $siteList.find('li.last');
 
-const hashMap = [
+const x = localStorage.getItem("x")
+
+const xObject = JSON.parse(x)
+
+const hashMap = xObject || [
     {logo: 'A', logType: "text", url: 'https://www.acfun.cn'},
     {logo: 'B', logType: "image", url: 'https://www.bilibili.com'}
 ];
 
+const simplifyUrl = (url) => {
+    return url.replace('https://', '')
+        .replace('http://', '')
+        .replace('www.', '')
+        .replace(/\/.*/, '') // 删除 / 开头的内容
+}
 
 const render = () => {
     $siteList.find('li:not(.last)').remove();
 
-    hashMap.forEach((node) => {
+    hashMap.forEach((node, index) => {
         const $li = $(`<li><a href="${node.url}">
                 <div class="site">
                     <div class="logo">${node.logo}</div>
-                    <div class="link">${node.url}</div>
+                    <div class="link">${simplifyUrl(node.url)}</div>
+                    <div class="close">
+                      <svg class="icon" aria-hidden="true">
+                         <use xlink:href="#icon-cangpeitubiao_shanchu"></use>
+                      </svg>
+                    </div>
                 </div>
             </a></li>`).insertBefore($lastLi);
+
+
+        $li.on('click', '.close', (e) => {
+            e.preventDefault()
+            console.log(hashMap)
+            hashMap.splice(index, 1)
+            render()
+        })
+
     });
 };
+
 
 render();
 
@@ -27,7 +52,7 @@ $('.addButton').on('click', () => {
     let url = window.prompt("请输入网站地址");
 
     hashMap.push({
-        logo: url[0],
+        logo: simplifyUrl(url)[0].toUpperCase(),
         logType: 'text',
         url: url
     });
@@ -35,3 +60,15 @@ $('.addButton').on('click', () => {
     render();
 
 });
+
+
+// $('.site').on('click','.close', (e) => {
+//     e.preventDefault()
+// })
+
+
+window.onbeforeunload = () => {
+    console.log("页面要关闭了");
+    const str = JSON.stringify(hashMap)
+    localStorage.setItem("x", str)
+}
